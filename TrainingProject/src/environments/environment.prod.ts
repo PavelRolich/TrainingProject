f0601eb25abd665f17e36090cc7d03878305edf9
Pavel <pahan.rolch@mail.ru>
2020-02-16 20:18:07 +0300

    connect to firebase

--- TrainingProject/src/environments/environment.prod.ts
+++ TrainingProject/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAJxfN6v9Mtwm_Pb2I_Hn1TYWKGh8wfbBc',
+    authDomain: 'fitnesstracker-15e07.firebaseapp.com',
+    databaseURL: 'https://fitnesstracker-15e07.firebaseio.com',
+    projectId: 'fitnesstracker-15e07',
+    storageBucket: 'fitnesstracker-15e07.appspot.com',
+    messagingSenderId: '166618200792',
+    appId: '1:166618200792:web:0c59cfa83b618576cdb5ad',
+    measurementId: 'G-BRNCFDXL3R'
+  }
 };
--- TrainingProject/src/environments/environment.ts
+++ TrainingProject/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAJxfN6v9Mtwm_Pb2I_Hn1TYWKGh8wfbBc',
+    authDomain: 'fitnesstracker-15e07.firebaseapp.com',
+    databaseURL: 'https://fitnesstracker-15e07.firebaseio.com',
+    projectId: 'fitnesstracker-15e07',
+    storageBucket: 'fitnesstracker-15e07.appspot.com',
+    messagingSenderId: '166618200792',
+    appId: '1:166618200792:web:0c59cfa83b618576cdb5ad',
+    measurementId: 'G-BRNCFDXL3R'
+  }
 };
 
 /*
